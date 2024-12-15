a862caafe88a916309401e5826398e0600477b9e
Pradipta Arya Daniswara <38103304+SomeRandomDolphin@users.noreply.github.com>
2024-12-15 11:29:11 +0700

    fix: fix malformed url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const PARTYKIT_HOST = process.env.NEXT_PUBLIC_PARTYKIT_HOST || "127.0.0.1:1999";
 const PARTYKIT_PROTOCOL =
-  PARTYKIT_HOST?.startsWith("localhost") ||
-  PARTYKIT_HOST?.startsWith("127.0.0.1")
+  PARTYKIT_HOST.startsWith("localhost") || PARTYKIT_HOST.startsWith("127.0.0.1")
     ? "http"
     : "https";
 const PARTYKIT_URL = `${PARTYKIT_PROTOCOL}://${PARTYKIT_HOST}`;
